742599d9be0beef657c7e06ad53c48a87364224c
mavroian <mavflorin@gmail.com>
2019-09-23 12:40:38 +0900

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Cocktail-Shacker-Algorithm-Visualization
+
 ## This was created during my time as a [Code Chrysalis](https://codechrysalis.io) Student
+
+### Installing
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+After you clone this reposotory type 'yarn' in your teminal to install dependencies.
+
+```
+$ yarn
+```
+
+--> Open index.html in a breowser of your choise
+--> Click on prepare coctail
+--> Click on shake
+
+### Enojoy the Coctail shaker sorting Visualisation
--- test/_sort.js
+++ test/_sort.js
@@ -1,23 +1,24 @@
-// const Sort = require("../src/Sort");
-// const { expect } = require("chai");
+const Sort = require("../src/Sort");
+const { expect } = require("chai");
 
-// describe("Cocktail Shacker", () => {
-//   it("should be a function", () => {
-//     expect(Sort).to.be.a("function");
-//   });
-//   it("should have a sort method", () => {
-//     let array = [9, 8, 6, 2, 4, 10, 7, 1, 3, 5];
-//     const Cocktail = new Sort(array);
-//     Cocktail.sort();
+//test will not work because sort algorithm is implemented to manipulate dom elements
+describe("Cocktail Shacker", () => {
+  it("should be a function", () => {
+    expect(Sort).to.be.a("function");
+  });
+  it("should have a sort method", () => {
+    let array = [9, 8, 6, 2, 4, 10, 7, 1, 3, 5];
+    const Cocktail = new Sort(array);
+    Cocktail.sort();
 
-//     expect(Sort.prototype.sort).to.be.a("function");
-//     expect(Cocktail.array).to.eql([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-//     it("should handle duplicates values ", () => {
-//       let array = [9, 8, 6, 2, 4, 10, 7, 1, 3, 5, 3];
-//       const Cocktail = new Sort(array);
-//       Cocktail.sort();
+    expect(Sort.prototype.sort).to.be.a("function");
+    expect(Cocktail.array).to.eql([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    it("should handle duplicates values ", () => {
+      let array = [9, 8, 6, 2, 4, 10, 7, 1, 3, 5, 3];
+      const Cocktail = new Sort(array);
+      Cocktail.sort();
 
-//       expect(Cocktail.array).to.eql([1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10]);
-//     });
-//   });
-// });
+      expect(Cocktail.array).to.eql([1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });
+  });
+});
